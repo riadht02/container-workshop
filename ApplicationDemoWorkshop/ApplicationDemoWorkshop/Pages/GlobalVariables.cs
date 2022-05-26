9adb61799e51dbef6ade5e85a6d186e22991154e
riadht02 <riadht@gmail.com>
2022-05-27 00:45:01 +0200

    add OS version

--- ApplicationDemoWorkshop/ApplicationDemoWorkshop/Pages/GlobalVariables.cs
+++ ApplicationDemoWorkshop/ApplicationDemoWorkshop/Pages/GlobalVariables.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ApplicationDemoWorkshop.Pages
 {
@@ -9,5 +10,7 @@ namespace ApplicationDemoWorkshop.Pages
     {
         public static int SessionCount = 0;
         public static string hostname = Environment.MachineName;
+        public static string osver = Environment.OSVersion.ToString();
     }
+
 }
