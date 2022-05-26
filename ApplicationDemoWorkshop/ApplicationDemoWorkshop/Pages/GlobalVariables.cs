8d575612bf9983cb4194494742e2e30c4da2bfdc
riadht02 <riadht@gmail.com>
2022-05-26 21:06:08 +0200

    add session counter

---
+++ ApplicationDemoWorkshop/ApplicationDemoWorkshop/Pages/GlobalVariables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationDemoWorkshop.Pages
+{
+    public class GlobalVariables
+    {
+        public static int SessionCount = 0;
+    }
+}
