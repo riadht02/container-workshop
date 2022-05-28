3060ca29bf1183967278da6880742f535095da01
Temala, Ridha (ext) <ridha.temala.ext@suez.com>
2022-05-28 13:44:17 +0200

    cpu usage

--- ApplicationDemoWorkshop/ApplicationDemoWorkshop/Pages/GlobalVariables.cs
+++ ApplicationDemoWorkshop/ApplicationDemoWorkshop/Pages/GlobalVariables.cs
@@ -11,6 +11,22 @@ namespace ApplicationDemoWorkshop.Pages
         public static int SessionCount = 0;
         public static string hostname = Environment.MachineName;
         public static string osver = Environment.OSVersion.ToString();
+
+        public static string GetCpuUsageForProcess()
+        {
+            var startTime = DateTime.UtcNow;
+            var startCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+            Task.Delay(500).Wait();
+
+            var endTime = DateTime.UtcNow;
+            var endCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+            var cpuUsedMs = (endCpuUsage - startCpuUsage).TotalMilliseconds;
+            var totalMsPassed = (endTime - startTime).TotalMilliseconds;
+            var cpuUsageTotal = cpuUsedMs / (Environment.ProcessorCount * totalMsPassed);
+            return string.Format("{0:P2}", cpuUsageTotal * 100) ;
+        }
     }
+
+   
 
 }
