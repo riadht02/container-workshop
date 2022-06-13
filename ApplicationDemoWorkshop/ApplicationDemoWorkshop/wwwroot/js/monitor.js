51e97124a085094c79632721dd40e949c747dff0
riadht02 <riadht@gmail.com>
2022-06-13 18:19:02 +0200

    make some vars static

--- ApplicationDemoWorkshop/ApplicationDemoWorkshop/wwwroot/js/monitor.js
+++ ApplicationDemoWorkshop/ApplicationDemoWorkshop/wwwroot/js/monitor.js
@@ -4,8 +4,6 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/monitorHub").build
 
 connection.on("ReceiveMessage", function (data) {
     document.getElementById("sessionCount").textContent = data.sessionCount;
-    document.getElementById("hostName").textContent = data.hostname;
-    document.getElementById("osversion").textContent = data.osVersion;
     document.getElementById("cpuUsage").textContent = data.cpuUsage;
 });
 
